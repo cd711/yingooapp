900aebadf38cce655b71e74db7326e54543acfc7
吕不为 <yblunan@gmail.com>
2020-10-20 17:18:37 +0800

    before type version update

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -3,7 +3,8 @@ import { View } from '@tarojs/components'
 import './index.less'
 
 export default class Index extends Component<{}, {
-  size?: {width: string; height: string};
+  size?: {width: string | number; height: string | number};
+  editorAnim?: boolean;
   data?: number;
 }> {
 
@@ -15,38 +16,51 @@ export default class Index extends Component<{}, {
     }
 
     onMsg: {(e: MessageEvent<any>): void} = ({data}) => {
-      if (data?.from == "editor") {
+      console.log("msg", data);
+      if (!data) {
+        return;
+      }
+      if (data.from == "editor") {
         switch (data.type) {
           case "mainSize":
-            this.setEditorSize(data.data);
+            setTimeout(()=>this.setEditorSize(data.data), 3000);
             break;
         }
       }
     }
 
     setEditorSize: {(size: {width: number; height: number}): void} = ({width, height}) => {
+      
       let w = width;
       let h = height;
-      if (w > 360) {
-        w = 360;
+      if (w > 270) {
+        w = 270;
         h = 360 / width * h;
       }
       if (h > 480) {
         h = 480;
         w = 480 / h * w;
       }
+      
+      // document.body.querySelector(".editor").addEventListener("")
       this.setState({
+        editorAnim: true,
         size: {
-          width: `${w}rpx`,
-          height: `${h}rpx`
+          width: Taro.pxTransform(w),
+          height: Taro.pxTransform(h)
         }
-      })
+      });
+      setTimeout(()=> this.setState({
+        editorAnim: false}), 3000);
     }
     render() {
-      const {size} = this.state;
+      const {size, editorAnim} = this.state;
+
+      
       return <View>
-        <View className='editor' style={size}>
-          <iframe src='http://localhost:8080/mobile?tpl_id=20200826&amp;id=12'></iframe>
+        <View className={`editor ${editorAnim ? ' anim' : ''}`} style={size ? {width: size.width, height: size.height} : undefined}>
+          {/* eslint-disable-next-line react/forbid-elements */}
+          <iframe className={`${editorAnim ? 'anim' : ''}`} src='http://192.168.0.100:8080/mobile?tpl_id=20200826&amp;id=12'></iframe>
         </View>
         </View>     
     }
