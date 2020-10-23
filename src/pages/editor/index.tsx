6bc6aa5520f6d4412ae6fac7d5c4bc2227013585
吕不为 <yblunan@gmail.com>
2020-10-23 15:26:58 +0800

    template load

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,12 +1,14 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
-import './index.less'
+import Taro, { Component } from '@tarojs/taro';
+import { View } from '@tarojs/components';
+import { AtActivityIndicator } from "taro-ui";
+import './index.less';
 
 
 export default class Index extends Component<{}, {
   size?: { width: string | number; height: string | number };
   editorAnim?: boolean;
   data?: number;
+  loadingTemplate?: boolean;
 }> {
 
   private tplId: string;
@@ -14,9 +16,12 @@ export default class Index extends Component<{}, {
 
   constructor(p) {
     super(p);
-    this.tplId = this.$router.params.query ? this.$router.params.query['tpl_id'] : '20200826';
-    this.productId = this.$router.params.query ? this.$router.params.query['id'] : '12';
-  
+    // console.log(this.$router.params);
+    this.tplId = this.$router.params['tpl_id'] || '20200826';
+    this.productId = this.$router.params['id'] || '12';
+    this.state = {
+      loadingTemplate: true
+    };
   }
 
   componentDidMount() {
@@ -41,6 +46,12 @@ export default class Index extends Component<{}, {
     }
     if (data.from == "editor") {
       switch (data.type) {
+        case "onload":
+          this.setState({
+            loadingTemplate: false
+          });
+          break;
+
         case "mainSize":
           setTimeout(() => this.setEditorSize(data.data), 3000);
           break;
@@ -70,11 +81,11 @@ export default class Index extends Component<{}, {
     });
     setTimeout(() => this.setState({
       editorAnim: false
-    }), 3000);
+    }), 1000);
   }
 
   render() {
-    const { size, editorAnim } = this.state;
+    const { loadingTemplate, size, editorAnim } = this.state;
 
     if (!this.tplId && !this.productId) {
       return null;
@@ -84,6 +95,7 @@ export default class Index extends Component<{}, {
       <View className={`editor ${editorAnim ? ' anim' : ''}`} style={size ? { height: size.height } : undefined}>
         {/* eslint-disable-next-line react/forbid-elements */}
         <iframe className={`${editorAnim ? 'anim' : ''}`} src={`http://192.168.0.100:8080/mobile?tpl_id=${this.tplId}&amp;id=${this.productId}`}></iframe>
+        {loadingTemplate ? <View className='loading'><AtActivityIndicator size={64} mode='center' /></View> : null}
       </View>
     </View>
   }
