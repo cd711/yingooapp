0a0dc5de1a50d0e50775f696807e2a65e2ae5321
吕不为 <yblunan@gmail.com>
2020-11-06 15:20:07 +0800

    devServer

--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,14 @@ module.exports = {
   defineConstants: {
   },
   mini: {},
-  h5: {}
+  h5: {
+    devServer: {
+      proxy: {
+        '/api/': {
+          target: "http://192.168.0.166",
+          changeOrigin: true
+        }
+      }
+    }
+  } 
 }
