6b215708f73ad23c37e70078f33c10ab4c8cb5d5
吕不为 <yblunan@gmail.com>
2020-11-09 16:33:10 +0800

    ss

--- src/pages/editor/shell.tsx
+++ src/pages/editor/shell.tsx
@@ -977,8 +977,7 @@ export default class Shell extends Component<{}, {
             </View>
             <View className="editor" style={size ? {height: size.height} : undefined}>
                 {/* eslint-disable-next-line react/forbid-elements */}
-                <iframe className="editor_frame"
-                        src={`http://192.168.0.166/editor/mobile?token=${getToken()}&tpl_id=${this.tplId}&doc_id=${this.docId}&t=999}`}></iframe>
+                <iframe className="editor_frame" src={`http://192.168.0.166/editor/mobile?token=${getToken()}&tpl_id=${this.tplId}&doc_id=${this.docId}&t=9998}`}></iframe>
                 {loadingTemplate ?
                     <View className='loading'><AtActivityIndicator size={64} mode='center'/></View> : null}
             </View>
