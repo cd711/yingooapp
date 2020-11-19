3855375de0f6b07889f685cb4cd26e1c7dd6fcd5
scc <scc_199608@163.com>
2020-11-19 15:31:25 +0800

    首页

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,13 @@
 import {ComponentType} from 'react'
 import Taro, {Component, Config} from '@tarojs/taro'
-import {Button, Image, Swiper, SwiperItem, Text, View} from '@tarojs/components'
+import {Button, Image, ScrollView, Swiper, SwiperItem, Text, View} from '@tarojs/components'
 import {inject, observer} from '@tarojs/mobx'
 import './index.less'
 import IconFont from '../../components/iconfont'
 import {api} from "../../utils/net";
-import {ossUrl} from "../../utils/common";
-import analyze from 'rgbaster'
+import {deviceInfo, ossUrl} from "../../utils/common";
+import RGBAster from 'rgbaster';
+import Fragment from "../../components/Fragment";
 
 const pics = [
     "https://i.ibb.co/sK68FQ0/c6a8dc33e8a84646b4cdc30f5cea391efc8a141c2bef0-UJ8-MBJ.jpg",
@@ -18,10 +19,10 @@ const pics = [
 ]
 
 interface IndexState {
-    tempColors: Array<string>;
+    currentColor: any[];
     colorWarp: string;
     swiperCurrent: number;
-    data: any
+    data: any[]
 }
 
 @inject("userStore")
@@ -42,49 +43,170 @@ class Index extends Component<any, IndexState> {
     constructor(props) {
         super(props);
         this.state = {
-            tempColors: [],
+            currentColor: [],
             colorWarp: "",
             swiperCurrent: 0,
-            data: {
-                special: [],
-                product: [],
-                tpl_product: []
-            }
+            data: []
         }
     }
 
-    componentDidMount() {
-        api("app.index/h5", {
-            area_size: JSON.stringify({
-                tpl_product: 3,
-                product: 9,
-                special: 6
-            })
-        }).then(data => {
-            this.setState({data})
-        }).catch(e => {
-            console.log("获取首页列表出错：", e)
-        })
-        this.test()
+    private total: number = 0;
+    getIndexList = async (data: {start?: number, size?: number, loadMore?: boolean} = {}) => {
+        const opt = {
+            start: data.start || 0,
+            size: data.size || 15,
+            loadMore: data.loadMore || false
+        };
+
+        try {
+            const res = await api("app.index/h5", {
+                start: opt.start,
+                size: opt.size
+            });
+            this.total = parseInt(res.total);
+            let arr = [];
+            if (opt.loadMore) {
+                arr = this.state.data.concat(res.list || []);
+            } else {
+                arr = res.list || [];
+            }
+            this.setState({data: arr})
+        }catch (e) {
+            console.log("首页获取列表出错：", e)
+        }
+
     }
 
-    test = () => {
-        // const colors = pics.map(async(item)=>{
-        //     const result = await analyze(item);
-        //     return result[0].color;
-        // });
-        // Promise.all(colors).then((results)=>{
-        //     console.log(results);  // [1, 2, 3]
-        //     this.setState({
-        //         tempColors:results,
-        //         colorWarp:results[0]
-        //     })
-        // });
+    componentDidMount() {
+        this.getIndexList()
+    }
 
-        analyze(pics[0]).then(res => {
-            console.log(res)
-        })
+    renderTemplateItem = (item, index) => {
+        let ele = null;
+        let currentColor = this.state.currentColor;
 
+        if (item.clist instanceof Array) {
+            // 商品模板
+            if (item.model === "tpl_product") {
+                if (item.clist.length > 0) {
+                    const itemLen = item.clist.length;
+                    if (itemLen === 1) {
+                        ele = (
+                            <View className='temp-warp' style={`background: #ff000030`} key={index}>
+                                <View style='background:rgba(0, 0, 0, 0.03);' className='mask'>
+                                    <View className='title'>
+                                        <Text className='txt'>{item.title}</Text>
+                                        {item.subtitle ? <Text className='sub-title'>{item.subtitle}</Text> : null}
+                                    </View>
+                                    <View className='swiper-back'>
+                                        <View className='temp-swiper'>
+                                            <Image src={ossUrl(item.clist[0].thumb_image, 1)} className='photo' mode='aspectFill'/>
+                                        </View>
+                                    </View>
+                                    <Button className='now-design-btn'>立即设计</Button>
+                                </View>
+                            </View>
+                        )
+                    } else if (itemLen > 1 && itemLen < 6) {
+                        // let temp = {
+                        //     [`${index}`]: {}
+                        // }
+                        // for (let i = 0; i < itemLen; i++) {
+                        //     temp[`${index}`] = {current: i, color: "transparent"};
+                        //     currentColor.push(temp)
+                        // }
+                        ele = (
+                            <View className='temp-warp' style={`background: #25632130`} key={index}>
+                                <View style='background:rgba(0, 0, 0, 0.03);' className='mask'>
+                                    <View className='title'>
+                                        <Text className='txt'>{item.title}</Text>
+                                        {item.subtitle ? <Text className='sub-title'>{item.subtitle}</Text> : null}
+                                    </View>
+                                    <View className='swiper-back'>
+                                        <Swiper className='temp-swiper' vertical autoplay={false} circular={false} indicatorDots={false}
+                                                onChange={({detail: {current}}) => {
+                                                    this.setState({
+                                                        colorWarp: this.state.tempColors[current],
+                                                        swiperCurrent: current
+                                                    })
+                                                }}
+                                        >
+                                            {
+                                                item.clist.map((child, cIdx) => (
+                                                    <SwiperItem key={`child_${cIdx}`}>
+                                                        <Image src={ossUrl(child.thumb_image, 1)} className='photo' mode='aspectFill'/>
+                                                    </SwiperItem>
+                                                ))
+                                            }
+                                        </Swiper>
+
+                                        <View className='indicatorDots'>
+                                            {
+                                                item.clist.map((_, index) => (
+                                                    <View className={this.state.swiperCurrent == index ? 'dot active' : 'dot'} key={index} />
+                                                ))
+                                            }
+                                        </View>
+                                    </View>
+                                    <Button className='now-design-btn'>立即设计</Button>
+                                </View>
+                            </View>
+                        )
+                    } else {
+                        ele = (
+                            <View className='temp-warp' key={index}>
+                                <View className='title'>
+                                    <Text className='txt'>{item.title}</Text>
+                                    {item.subtitle ? <Text className='sub-title'>{item.subtitle}</Text> : null}
+                                </View>
+                                <View className='grid'>
+                                    {
+                                        item.clist.slice(1, 7).map((child, cIdx) => (
+                                            <View className='photo-warp' key={`tel_${cIdx}`}>
+                                                <Image src={ossUrl(child.thumb_image, 1)} className='photo' mode='aspectFill'/>
+                                            </View>
+                                        ))
+                                    }
+                                </View>
+                                {
+                                    item.clist.length > 6
+                                        ? <View className='seemore'>
+                                            <Text className='txt'>查看更多</Text>
+                                            <IconFont name='20_xiayiye' size={40} color='#9C9DA6'/>
+                                        </View>
+                                        : null
+                                }
+                            </View>
+                        )
+                    }
+                }
+            } else if (item.model === "product") {  // 商品
+                ele = <Fragment>
+                    {
+                        item.clist.map((product, prodIndex) => (
+                            <View className='product-item' key={prodIndex}>
+                                <Image src={ossUrl(product.thumb_image, 1)} className='image' mode='aspectFill'/>
+                                <View className='bottom'>
+                                    <View className='left'>
+                                        <Text className='title'>{product.title || " "}</Text>
+                                        {product.subtitle ? <Text className='subtitle'>{product.subtitle}</Text> : null}
+                                    </View>
+                                    <View className='right-btn' onClick={() => {
+                                        // value.info.jump_url
+                                    }}>
+                                        <Text className='txt'>我要定制</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        ))
+                    }
+                </Fragment>
+            }
+        }
+
+        console.log("初始化的色值：", currentColor)
+        // this.setState({currentColor})
+        return ele
     }
 
 
@@ -98,94 +220,13 @@ class Index extends Component<any, IndexState> {
                         <Text className='placeholders'>搜索海量模板</Text>
                     </View>
                 </View>
-                <View className='container'>
-                    <View className='temp-warp'>
-                        <View className='title'>
-                            <Text className='txt'>大胆的色彩和创意</Text>
-                            <Text className='sub-title'>将色彩与创意做到极致</Text>
-                        </View>
-                        <View className='grid'>
-                            {
-                                data.special.map((item, index) => (
-                                    <View className='photo-warp' key={index}>
-                                        <Image src={ossUrl(item.thumb_image, 1)} className='photo' mode='aspectFill'/>
-                                    </View>
-                                ))
-                            }
-                        </View>
-                        <View className='seemore'>
-                            <Text className='txt'>查看更多</Text>
-                            <IconFont name='20_xiayiye' size={40} color='#9C9DA6'/>
-                        </View>
+                <ScrollView scrollY style={{height: deviceInfo.windowHeight - 102}}>
+                    <View className='inde_page_container'>
+                        {
+                            data.map((value, index) => this.renderTemplateItem(value, index))
+                        }
                     </View>
-                    <View className='temp-warp' style={`background: ${colorWarp}`}>
-                        <View style='background:rgba(0, 0, 0, 0.03);' className='mask'>
-                            <View className='title'>
-                                <Text className='txt'>大胆的色彩和创意</Text>
-                                <Text className='sub-title'>将色彩与创意做到极致</Text>
-                            </View>
-                            <View className='swiper-back'>
-                                <Swiper
-                                    className='temp-swiper'
-                                    vertical
-                                    circular={false}
-                                    indicatorDots={false}
-                                    onChange={({detail: {current}}) => {
-                                        console.log(current)
-                                        this.setState({
-                                            colorWarp: this.state.tempColors[current],
-                                            swiperCurrent: current
-                                        })
-                                    }}
-                                >
-                                    {
-                                        data.product.map((item, index) => (
-                                            <SwiperItem key={index}>
-                                                <Image src={ossUrl(item.thumb_image, 1)} className='photo' mode='aspectFill'/>
-                                            </SwiperItem>
-                                        ))
-                                    }
-                                </Swiper>
-
-                                <View className='indicatorDots'>
-                                    {
-                                        data.product.map((_, index) => (
-                                            <View className={swiperCurrent == index ? 'dot active' : 'dot'} key={index} />
-                                        ))
-                                    }
-                                </View>
-                            </View>
-                            {/* <View className='coverflow'>
-                                {
-                                    pics.map((item,index)=>(
-                                        <View className='item' key={index}>
-                                            <Image src={item} className='photo' mode='aspectFill'/>
-                                        </View>
-                                    ))
-                                }
-                            </View> */}
-                            <Button className='now-design-btn'>立即设计</Button>
-                        </View>
-                    </View>
-                    {
-                        data.tpl_product.map((value, index) => (
-                            <View className='product-item' key={index}>
-                                <Image src={ossUrl(value.thumb_image, 1)} className='image' mode='aspectFill'/>
-                                <View className='bottom'>
-                                    <View className='left'>
-                                        <Text className='title'>{value.title || " "}</Text>
-                                        <Text className='subtitle'>定制你的专属耐克</Text>
-                                    </View>
-                                    <View className='right-btn' onClick={() => {
-                                        // value.info.jump_url
-                                    }}>
-                                        <Text className='txt'>我要定制</Text>
-                                    </View>
-                                </View>
-                            </View>
-                        ))
-                    }
-                </View>
+                </ScrollView>
             </View>
         )
     }
