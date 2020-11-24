c1ed08f716517a6f4316dc15ae4599496e9e6dac
racall <xexiaoyi@gmail.com>
2020-11-24 18:49:03 +0800

    0

--- src/utils/ext.tsx
+++ src/utils/ext.tsx
@@ -19,49 +19,45 @@ const page =(option?: {
             const {code} = this.$router.params;
             option.redirectUrl = option.redirectUrl ? option.redirectUrl : window.location.href
             if (is_weixin() && userStore.id == null) {
+                if (code && code.length>5) {
+                    const params = this.$router.params;
+                    let exportUrl = window.location.href.split("?")[0]+(Object.keys(params).length>0?"?":"");
+                    Object.keys(params).map((key)=>{
+                        if (key != "code" && key != "state") {
+                            exportUrl += key + '=' + params[key] +'&';
+                        }
+                    })
+                    exportUrl = exportUrl[exportUrl.length-1]==='&' ? exportUrl.substring(0,exportUrl.length-2):exportUrl;
+                    window.history.replaceState(null,null,exportUrl)
+                    Taro.showLoading({title:"登录中..."});
+                    api("user/third",{
+                        platform:"wechat",
+                        code:code,
+                    }).then((res)=>{
+                        userStore.setInfo(res)
+                        Taro.hideLoading();
+                        Taro.showToast({
+                            title:"登录成功",
+                            icon:'none',
+                            duration:2000
+                        })
+                        location.replace(exportUrl)
+                        this.wechatLoginSuccess && this.wechatLoginSuccess(res);
+                    }).catch((e)=>{
+                        Taro.hideLoading();
+                        Taro.reLaunch({
+                            url:'/pages/index/index'
+                        });
+                    })
+                }
                 if (option.wechatAutoLogin) {
-                    if (code && code.length>5) {
-                        const params = this.$router.params;
-                        let exportUrl = window.location.href.split("?")[0]+(Object.keys(params).length>0?"?":"");
-                        Object.keys(params).map((key)=>{
-                            if (key != "code" && key != "state") {
-                                exportUrl += key + '=' + params[key] +'&';
-                            }
-                        })
-                        exportUrl = exportUrl[exportUrl.length-1]==='&' ? exportUrl.substring(0,exportUrl.length-2):exportUrl;
-                        window.history.replaceState(null,null,exportUrl)
-                        Taro.showLoading({title:"登录中..."});
-                        api("user/third",{
-                            platform:"wechat",
-                            code:code,
-                        }).then((res)=>{
-                            userStore.setInfo(res)
-                            Taro.hideLoading();
-                            Taro.showToast({
-                                title:"登录成功",
-                                icon:'none',
-                                duration:2000
-                            })
-                            location.replace(exportUrl)
-                            this.wechatLoginSuccess && this.wechatLoginSuccess(res);
-                        }).catch((e)=>{
-                            Taro.hideLoading();
-                            Taro.reLaunch({
-                                url:'/pages/index/index'
-                            });
-                        })
-                    } else {
+                    if(code == undefined || code == null || !code) {
                         // @ts-ignore
                         const url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${common_config.wxappid}&redirect_uri=${encodeURIComponent(option.redirectUrl)}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`;
                         window.location.href = url;
                     }
                 }
             }
-            // if (userStore.id == null) {
-            //     Taro.reLaunch({
-            //         url:'/pages/index/index'
-            //     });
-            // }
         }
         clazz.componentWillMount = function () {
             this.__option = {};
