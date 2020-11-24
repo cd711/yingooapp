d71ef92680d83e12012e0f826a5e57af6aa5cc94
scc <scc_199608@163.com>
2020-11-24 17:08:17 +0800

    登录窗口更新

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,32 +1,94 @@
-import Taro from "@tarojs/taro";
-import {View, Text, Image} from "@tarojs/components";
+import Taro, {useState, useEffect} from "@tarojs/taro";
+import {View, Text} from "@tarojs/components";
 import IconFont from "../iconfont";
 import "./login.less"
+import {is_weixin} from "../../utils/common";
 
 interface LoginProps {
     onClose?: () => void;
     onOk?: () => void;
+    // 第三方登录点击
+    onThirdPartyAuth?: (type: string) => void;
     showClose?: boolean;
+    loginTxt?: string
 }
 const Login: Taro.FC<LoginProps> = props => {
 
-    const {onOk, onClose,
-        showClose = true
+    const {onOk, onClose, onThirdPartyAuth,
+        showClose = true,
+        loginTxt = "手机号登录"
     } = props;
 
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        setVisible(true)
+    }, [])
+
+    const privacyView = type => {
+        onClose && onClose()
+        Taro.navigateTo({url: `/pages/me/privacy?pageType=${type}`})
+    }
+
+    const _onClose = () => {
+        setVisible(false)
+        setTimeout(() => {
+            onClose && onClose()
+        }, 200)
+    }
+
     return (
-        <View className="un_login_model">
-            <View className="model_main">
-                <Image src={require("../../source/login.png")} className="login_bg" />
-                <View className="model_info_main">
-                    <Text className="h2">您还未登录</Text>
-                    <Text className="txt">请先登录/注册再进行此操作</Text>
-                    <View className="btn" onClick={onOk}><Text className="lo_txt">登录</Text></View>
+        <View className={`un_login_model ${visible ? "animtion_un_login_model" : ""}`}>
+            <View className={`login_model_main ${visible ? "animtion_login_model" : ""}`}>
+                <View className="head">
+                    <Text className="h2">登录后体验全面功能</Text>
+                    {showClose ? <View className="close" onClick={_onClose}><IconFont name="24_guanbi" size={40} /></View> : null}
+                </View>
+                <View className="login_btn">
+                    <View className="btn_lo" onClick={onOk}>
+                        <Text className="txt">{loginTxt}</Text>
+                    </View>
+                </View>
+                <View className="oth"><Text className="exp">其他方式</Text></View>
+                <View className="other_login_way">
+                    <View className="item" onClick={() => onThirdPartyAuth("qq")}>
+                        <IconFont name="24_QQ-Color" size={48}/><Text className="name">QQ登录</Text>
+                    </View>
+                    {
+                        is_weixin()
+                            ? <View className="item" onClick={() => onThirdPartyAuth("wechat")}>
+                                <IconFont name="24_weixin-Color" size={48}/><Text className="name">微信登录</Text>
+                            </View>
+                            : null
+                    }
+                </View>
+                <View className="priv_item">
+                    <Text className="txt">登录即同意<Text className="link" onClick={() => privacyView("user_agreement")}>《映果用户协议》</Text>和<Text className="link" onClick={() => privacyView("privacy")}>《映果隐私协议》</Text></Text>
                 </View>
             </View>
-            {showClose ? <View className="close" onClick={onClose}><IconFont name="16_qingkong" size={72} /></View> : null}
         </View>
     )
 }
+
+// const Login: Taro.FC<LoginProps> = props => {
+//
+//     const {onOk, onClose,
+//         showClose = true
+//     } = props;
+//
+//     return (
+//         <View className="un_login_model">
+//             <View className="model_main">
+//                 <Image src={require("../../source/login.png")} className="login_bg" />
+//                 <View className="model_info_main">
+//                     <Text className="h2">您还未登录</Text>
+//                     <Text className="txt">请先登录/注册再进行此操作</Text>
+//                     <View className="btn" onClick={onOk}><Text className="lo_txt">登录</Text></View>
+//                 </View>
+//             </View>
+//             {showClose ? <View className="close" onClick={onClose}><IconFont name="16_qingkong" size={72} /></View> : null}
+//         </View>
+//     )
+// }
 
 export default Login
