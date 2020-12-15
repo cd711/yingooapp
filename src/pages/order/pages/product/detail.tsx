0a44b85080162db125a5440c2bbdcbbc40c08d80
Brian He <i@ihitun.com>
2020-12-15 18:52:57 +0800

    0

--- src/pages/order/pages/product/detail.tsx
+++ src/pages/order/pages/product/detail.tsx
@@ -1,7 +1,7 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text,Swiper, SwiperItem,ScrollView,Image } from '@tarojs/components'
+import { View, Text,Swiper, SwiperItem,ScrollView,Image,Button } from '@tarojs/components'
 import IconFont from '../../../../components/iconfont';
-import { deviceInfo,fixStatusBarHeight, ossUrl } from '../../../../utils/common';
+import { deviceInfo,fixStatusBarHeight, notNull, ossUrl } from '../../../../utils/common';
 import {api} from '../../../../utils/net';
 import './detail.less'
 
@@ -40,9 +40,22 @@ export default class Login extends Component<{},{
             currentPreImageIndex:current
         })
     }
+    onCollected = () => {
+        
+    }
+    onAddCart= () => {
+
+    }
+    onNowBuy = () => {
+
+    }
     render() {
         const {data,currentPreImageIndex} = this.state;
         const image:Array<any> = data && data.image && data.image.length>0?data.image:[];
+        const flag_text:Array<any> = data && !notNull(data.flag_text) ? data.flag_text:[];
+        const tags_text:Array<any> = data && !notNull(data.tags_text) ? data.tags_text.slice(0,4):[];
+        const attrGroup:Array<any> = data && !notNull(data.attrGroup) ? data.attrGroup:[];
+        console.log()
         return (
             <View className='p_detail'>
                 {/* @ts-ignore */}
@@ -78,31 +91,36 @@ export default class Login extends Component<{},{
                     <View className="product_info">
                         <View className='title'>
                             <Text className='txt'>{data.title}</Text>
-                            <View className='hot'>
-                                <Image className='icon' src={require("../../../../source/hot.png")} />
-                                <Text className='rx'>热销</Text>
-                            </View>
+                            {
+                                flag_text.map((item,index)=>(
+                                    <View className='hot' key={index+""}>
+                                        <Image className='icon' src={require("../../../../source/hot.png")} />
+                                        <Text className='rx'>{item}</Text>
+                                    </View>
+                                ))
+                            }
                         </View>
 
                         <View className='tags'>
-                            <View className='item'>
-                                <Text className='txt'>可加塑封</Text>
-                            </View>
-                            <View className='item'>
-                                <Text className='txt'>皮革</Text>
-                            </View>
+                            {
+                                tags_text.map((item)=>(
+                                    <View className='item' key={item.id}>
+                                        <Text className='txt'>{item.name}</Text>
+                                    </View>
+                                ))
+                            }
                         </View>
                         
                         <View className='price_line'>
                             <View className='dp'>
                                 <Text className='smy'>￥</Text>
-                                <Text className='num'>49</Text>
+                                <Text className='num'>{data.price || "0.00"}</Text>
                             </View>
                             <View className='ap'>
-                                <Text className='txt'>￥99.9</Text>
+                                <Text className='txt'>￥{data.market_price || "0.00"}</Text>
                             </View>
                             <View className='total'>
-                                <Text className='txt'>43人已抢</Text>
+                                <Text className='txt'>{data.sold_count || 0}人已抢</Text>
                             </View>
                         </View>
                     </View>
@@ -110,12 +128,23 @@ export default class Login extends Component<{},{
                         <View className='item'>
                             <View className='content'>
                                 <Text className='title'>规格</Text>
-                                <Text className='sku'>选择 XXX</Text>
+                                <Text className='sku'>选择 {attrGroup.map((item)=>item["name"]).join("")}</Text>
                             </View>
                             <IconFont name='20_xiayiye' size={40} color='#9C9DA6' />
                         </View>
                     </View>
                 </ScrollView>
+                <View className='product_bottom_bar'>
+                    <View className='cart' onClick={()=>Taro.switchTab({url:'/pages/tabbar/cart/index'})}>
+                        <IconFont name={'24_gouwuche'} size={48} color={'#707177'} />
+                        <Text className='txt'>购物车</Text>
+                    </View>
+                    <View className='ops'>
+                        <Button className='add-cart-btn' onClick={this.onAddCart}>加入购物车</Button>
+                        <Button className='now-buy-btn' onClick={this.onNowBuy}>立即购买</Button>
+                    </View>
+                </View>
+
             </View>
         )
     }
