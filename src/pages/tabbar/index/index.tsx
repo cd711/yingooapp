d0bc6d4a6eeae14ce97a3dc39f90d470d42abdfc
Brian He <i@ihitun.com>
2020-12-30 10:05:09 +0800

    0

--- src/pages/tabbar/index/index.tsx
+++ src/pages/tabbar/index/index.tsx
@@ -163,7 +163,7 @@ class Index extends Component<any, IndexState> {
         })
     }
     componentDidShow() {
-        setTempDataContainer("product_preview_sku",null,()=>{});
+
     }
     onItemClick = (item, _) => {
         console.log(item)
