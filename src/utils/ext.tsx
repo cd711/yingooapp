bbe941568be2c6f9d231e73dd38c5dc92a6095b7
Brian He <i@ihitun.com>
2020-12-30 18:02:16 +0800

    0

--- src/utils/ext.tsx
+++ src/utils/ext.tsx
@@ -24,7 +24,7 @@ const page = (option?: {
                 return {
                     title: "免费照片冲印个性化定制手机壳",
                     path: "/pages/tabbar/index/index",
-                    imageUrl:"https://cdn.playbox.yingoo.com/uploads/file/20201230/18d71f9418c8ef78d72e6b7b8e81e460.png?x-oss-process=style/m"
+                    imageUrl:"https://cdn.playbox.yingoo.com/uploads/file/20201230/10a88cd83a5c6d2235d9829a56260281.png?x-oss-process=style/m"
                 };
             };
         }
