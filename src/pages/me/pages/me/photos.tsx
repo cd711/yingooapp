3533116bc3d4a89adfb71344de330bffe529c314
scc <scc_199608@163.com>
2020-12-30 16:56:52 +0800

    修复素材库的样式问题

--- src/pages/me/pages/me/photos.tsx
+++ src/pages/me/pages/me/photos.tsx
@@ -1,11 +1,11 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import {View, Text, Image, Button, ScrollView} from '@tarojs/components'
+import Taro, {Component, Config} from '@tarojs/taro'
+import {Button, Image, ScrollView, Text, View} from '@tarojs/components'
 import './photos.less'
 import IconFont from '../../../../components/iconfont';
 import {AtActivityIndicator, AtModal} from 'taro-ui'
 import {api} from "../../../../utils/net";
 import UploadFile from "../../../../components/Upload/Upload";
-import {ossUrl, deviceInfo} from "../../../../utils/common";
+import {deviceInfo, ossUrl, photoGetItemStyle} from "../../../../utils/common";
 import LoadMore from "../../../../components/listMore/loadMore";
 import Popover, {PopoverItemClickProps, PopoverItemProps} from "../../../../components/popover";
 import {ScrollViewProps} from "@tarojs/components/types/ScrollView";
@@ -15,7 +15,7 @@ import moment from "moment";
 
 
 interface PhotosState {
-    navSwitchActive:number;
+    navSwitchActive: number;
     loading: boolean;
     imageList: any[];
     videoList: any[];
@@ -28,15 +28,17 @@ interface PhotosState {
     editSelectImgIds: any[];
     editSelectAttr: string[];
 }
+
 @observer
-export default class Photos extends Component<{ }, PhotosState> {
+export default class Photos extends Component<{}, PhotosState> {
 
     config: Config = {
         navigationBarTitleText: '首页'
     }
 
     private scrollView: ScrollViewProps;
-    constructor(props){
+
+    constructor(props) {
         super(props);
         this.state = {
             navSwitchActive: 0,
@@ -73,7 +75,7 @@ export default class Photos extends Component<{ }, PhotosState> {
         if (data.order) {
             Object.assign(temp, {order: data.order})
         }
-        try{
+        try {
             const res = await api("app.profile/imgs", temp);
             this.total = Number(res.total);
             console.log(res);
@@ -81,12 +83,12 @@ export default class Photos extends Component<{ }, PhotosState> {
             let list = [];
             if (opt.type === 0) {
                 list = opt.loadMore ? this.state.imageList.concat(res.list) : res.list;
-                this.setState({imageList : list, loadStatus: Number(res.total) === list.length ? "noMore" : "more"})
+                this.setState({imageList: list, loadStatus: Number(res.total) === list.length ? "noMore" : "more"})
             } else {
                 list = opt.loadMore ? this.state.videoList.concat(res.list) : res.list;
                 this.setState({videoList: list, loadStatus: Number(res.total) === list.length ? "noMore" : "more"})
             }
-        }catch (e) {
+        } catch (e) {
             console.log("获取图库出错：", e)
             this.setState({loadStatus: "noMore"})
         }
@@ -125,7 +127,7 @@ export default class Photos extends Component<{ }, PhotosState> {
             } else {
                 console.log("选图没有本地存储")
             }
-        }catch (e) {
+        } catch (e) {
             console.log("选图本地存储失败")
         }
     }
@@ -191,7 +193,7 @@ export default class Photos extends Component<{ }, PhotosState> {
             await api("app.profile/delImgs", {ids: selects.join(",")});
             this.setState({selects: []})
             this.getList({start: 0})
-        }catch (e) {
+        } catch (e) {
             console.log("删除出错：", e)
         }
         this.setState({isOpened: false})
@@ -261,16 +263,16 @@ export default class Photos extends Component<{ }, PhotosState> {
     ]
 
     getScrollHeight = () => {
-        const {editSelectImgIds, imageList, videoList,navSwitchActive} = this.state;
+        const {editSelectImgIds, imageList, videoList, navSwitchActive} = this.state;
         const list = navSwitchActive === 0 ? imageList : videoList;
         const h = (list.length > 0) && editSelectImgIds.length > 0 ? deviceInfo.windowHeight - 130 - 45 : deviceInfo.windowHeight - 45;
         return deviceInfo.env === "h5" ? h : h + deviceInfo.menu.height + 5
     }
 
     render() {
-        const { navSwitchActive, loading, imageList, selects, videoList, loadStatus, isEdit, isOpened} = this.state;
+        const {navSwitchActive, loading, imageList, selects, videoList, loadStatus, isEdit, isOpened} = this.state;
         const list = navSwitchActive === 0 ? imageList : videoList;
-        const tabs = ["图片","视频"];
+        const tabs = ["图片", "视频"];
         return (
             <View className='photos'>
                 <View className='photos_nav_bar' style={{
@@ -279,13 +281,14 @@ export default class Photos extends Component<{ }, PhotosState> {
                     <View className='left' onClick={() => {
                         Taro.navigateBack()
                     }}>
-                        <IconFont name='24_shangyiye' size={48} color='#121314' />
+                        <IconFont name='24_shangyiye' size={48} color='#121314'/>
                     </View>
                     <View className='center'>
                         <View className='nav-switch'>
                             {
-                                tabs.map((item,index)=>(
-                                    <View className={navSwitchActive==index?'item active':'item'} key={index+""} onClick={() => this.changeType(index)}>
+                                tabs.map((item, index) => (
+                                    <View className={navSwitchActive == index ? 'item active' : 'item'} key={index + ""}
+                                          onClick={() => this.changeType(index)}>
                                         <Text className='txt'>{item}</Text>
                                     </View>
                                 ))
@@ -310,7 +313,7 @@ export default class Photos extends Component<{ }, PhotosState> {
                         {
                             list.length === 0
                                 ? <View className='empty'>
-                                    <Image src={require('../../../../source/empty/nophoto.png')} className='img' />
+                                    <Image src={require('../../../../source/empty/nophoto.png')} className='img'/>
                                     <Text className='txt'>暂无素材</Text>
                                     <UploadFile extraType={navSwitchActive === 0 ? 3 : 4}
                                                 uploadType={navSwitchActive === 0 ? "image" : "video"}
@@ -325,7 +328,8 @@ export default class Photos extends Component<{ }, PhotosState> {
                                     {
                                         deviceInfo.env === "weapp"
                                             ? <View className="list_filter">
-                                                <Text className="tit" onClick={() => this.onEdit(!isEdit)}>{isEdit ? "完成" : "管理"}</Text>
+                                                <Text className="tit"
+                                                      onClick={() => this.onEdit(!isEdit)}>{isEdit ? "完成" : "管理"}</Text>
                                                 {
                                                     isEdit
                                                         ? <View className="weapp_list_filter_act">
@@ -353,23 +357,31 @@ export default class Photos extends Component<{ }, PhotosState> {
                                                 type="card"
                                                 count={9}
                                                 uploadType={navSwitchActive === 0 ? "image" : "video"}
-                                                style={{width: "248rpx"}}
+                                                style={photoGetItemStyle()}
                                                 onChange={this.uploadFile}/>
                                         </View>
                                         {
                                             list.map((item, idx) => {
-                                                return <View className="list_item" key={idx+""}>
-                                                    <View className="img_item" key={idx+""}
+                                                return <View className="list_item" key={idx + ""}>
+                                                    <View className="img_item" key={idx + ""}
+                                                          style={photoGetItemStyle()}
                                                           onClick={() => this.imageSelect(item.id)}>
-                                                        <Image src={item.imagetype === "video" ? `${item.url}?x-oss-process=video/snapshot,t_1000,w_360,h_0,f_jpg,m_fast` : ossUrl(item.url, 1)}
+                                                        <Image src={item.imagetype === "video"
+                                                            ? `${item.url}?x-oss-process=video/snapshot,t_1000,w_360,h_0,f_jpg,m_fast`
+                                                            : ossUrl(item.url, 1)}
                                                                mode="aspectFill"
+                                                               style={photoGetItemStyle()}
                                                                className="img"/>
                                                     </View>
                                                     {
                                                         isEdit
                                                             ? <View className="act_btn">
-                                                                <IconFont name={selects.indexOf(Number(item.id)) > -1 ? "22_yixuanzhong" : "22_touming-weixuanzhong"}
-                                                                          size={44} />
+                                                                <IconFont
+                                                                    name={selects.indexOf(Number(item.id)) > -1
+                                                                        ? "22_yixuanzhong"
+                                                                        : "22_touming-weixuanzhong"
+                                                                    }
+                                                                    size={44}/>
                                                             </View>
                                                             : null
                                                     }
@@ -379,20 +391,23 @@ export default class Photos extends Component<{ }, PhotosState> {
                                     </View>
                                 </View>
                         }
-                        {list.length > 0 ? <LoadMore status={loadStatus} /> : null}
+                        {list.length > 0 ? <LoadMore status={loadStatus}/> : null}
                     </ScrollView>
-                    {loading ? <AtActivityIndicator mode='center' /> : null}
+                    {loading ? <AtActivityIndicator mode='center'/> : null}
                     {
                         isEdit
                             ? <View className="select_all_container">
                                 <View className="left">
                                     <View onClick={this.selectAll}>
-                                        <IconFont name={selects.length === list.length ? "22_yixuanzhong" : "22_touming-weixuanzhong"} size={44}/>
+                                        <IconFont
+                                            name={selects.length === list.length ? "22_yixuanzhong" : "22_touming-weixuanzhong"}
+                                            size={44}/>
                                         <Text>全选</Text>
                                     </View>
                                 </View>
                                 <View className="right">
-                                    <View className="btn" onClick={this.onDeleteSelect}>删除{selects.length > 0 ? `(${selects.length})` : null}</View>
+                                    <View className="btn"
+                                          onClick={this.onDeleteSelect}>删除{selects.length > 0 ? `(${selects.length})` : null}</View>
                                 </View>
                             </View>
                             : null
