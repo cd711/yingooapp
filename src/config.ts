7caa1c3ec525e8126bad743b3969a35fe614478d
scc <scc_199608@163.com>
2021-01-21 16:05:34 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,6 @@ if (process.env.TARO_ENV == "h5") {
     // @ts-ignore
     // eslint-disable-next-line no-undef
     productionUrl = common_config.url ? common_config.url : productionUrl;
-    // 11111111
 }
 const config = {
     apiUrl: process.env.NODE_ENV !== 'production' ? testApiUrl : productionUrl,
@@ -12,7 +11,7 @@ const config = {
     // 静态资源地址
     sourceUrl: "https://cdn.playbox.yingoo.com/",
     // 编辑器本地地址
-    editorUrl: process.env.NODE_ENV !== 'production' ? "https://test.playbox.yingoo.com" : "https://561454564564564564",
+    editorUrl: process.env.NODE_ENV !== 'production' ? "https://test.playbox.yingoo.com" : "https://m.playbox.yingoo.com",
     // editorUrl:"https://yin.gaozhenzhen.com",
     // 本地H5地址
     h5Url: process.env.NODE_ENV !== 'production' ? "https://test.playbox.yingoo.com" : "https://m.playbox.yingoo.com",
