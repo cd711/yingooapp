e278cb3aa838a20bfe4536b44e001a328fcb5f93
scc <scc_199608@163.com>
2021-01-21 16:04:44 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ const config = {
     // 静态资源地址
     sourceUrl: "https://cdn.playbox.yingoo.com/",
     // 编辑器本地地址
-    editorUrl: process.env.NODE_ENV !== 'production' ? "https://test.playbox.yingoo.com" : "https://m.playbox.yingoo.com",
+    editorUrl: process.env.NODE_ENV !== 'production' ? "https://test.playbox.yingoo.com" : "https://561454564564564564",
     // editorUrl:"https://yin.gaozhenzhen.com",
     // 本地H5地址
     h5Url: process.env.NODE_ENV !== 'production' ? "https://test.playbox.yingoo.com" : "https://m.playbox.yingoo.com",
