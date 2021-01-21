cfefc2fab68a4d1577d5e09a16a6bd9a85437069
Brian He <i@ihitun.com>
2021-01-21 15:50:58 +0800

    0

--- src/app.tsx
+++ src/app.tsx
@@ -15,7 +15,7 @@ import wx from 'weixin-js-sdk'
 // const zhuge_app = require("./utils/zhuge-wx.min.js")
 
 // 如果需要在 h5 环境中开启 React Devtools
-// 取消以下注释：
+// 取消以下注释：dev分支下的测试
 // if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
 //   require('nerv-devtools')
 // }
