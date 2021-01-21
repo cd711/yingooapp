685af4f60c4072f5975255e9c4a503d66373752c
scc <scc_199608@163.com>
2021-01-21 15:59:13 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ if (process.env.TARO_ENV == "h5") {
     // @ts-ignore
     // eslint-disable-next-line no-undef
     productionUrl = common_config.url ? common_config.url : productionUrl;
+    //
 }
 const config = {
     apiUrl: process.env.NODE_ENV !== 'production' ? testApiUrl : productionUrl,
