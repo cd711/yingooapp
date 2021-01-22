d7db596ab146fe8e206fd38b1d447626ca211380
scc <scc_199608@163.com>
2021-01-22 11:21:12 +0800

    文件中转站

---
+++ src/components/documentTransfer/index.tsx
@@ -0,0 +1,16 @@
+import "./index.less";
+import Taro, {useState, useEffect, useRef} from "@tarojs/taro";
+import {View} from "@tarojs/components";
+
+const DocumentTransfer: Taro.FC<any> = props => {
+
+    return (
+        <View className="document_transfer_container">
+            <div className="document_transfer_main">
+
+            </div>
+        </View>
+    )
+}
+
+export default DocumentTransfer
